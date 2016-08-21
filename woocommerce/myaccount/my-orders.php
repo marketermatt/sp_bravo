151592cb190c65e58e7155c4b7b99e9dd9cbb965
gaganphp <mailtogagan990@gmail.com>
2016-08-22 05:01:34 +0530

    updated woocommerce templates to latest version

--- woocommerce/myaccount/my-orders.php
+++ woocommerce/myaccount/my-orders.php
@@ -2,13 +2,7 @@
 /**
  * My Orders
  *
- * Shows recent orders on the account page
- *
- * actual version 2.2.0
- *
- * @author 		WooThemes
- * @package 	WooCommerce/Templates
- * @version     5.0.0
+ * @deprecated  2.6.0 this template file is no longer used. My Account shortcode uses orders.php.
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
